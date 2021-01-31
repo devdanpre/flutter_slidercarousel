d0dbddea7b3feec446d7a56db540605877398a1d
Daniel Preciado <daniel.preciado@eventools.mx>
2021-01-30 23:53:59 -0600

    Update flutter_slidercarousel_test.dart

--- test/flutter_slidercarousel_test.dart
+++ test/flutter_slidercarousel_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_slidercarousel/flutter_slidercarousel.dart';
 
 void main() {
-  testWidgets('Default Swiper', (WidgetTester tester) async {
+  testWidgets('Default SliderCarousel', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
-        home: Swiper(
+        home: SliderCarousel(
             itemBuilder: (context, index) {
               return Text("0");
             },
@@ -15,38 +15,6 @@ void main() {
     expect(find.text("0", skipOffstage: false), findsOneWidget);
   });
 
-  testWidgets('Default Swiper loop:false', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
-        home: Swiper(
-          itemCount: 10,
-          loop: false,
-        )));
 
-    expect(find.text("0", skipOffstage: true), findsOneWidget);
-  });
-
-  testWidgets('Create Swiper with children', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
-        home: Swiper.children(
-          children: <Widget>[Text("0"), Text("1")],
-        )));
-
-    expect(find.text("0", skipOffstage: false), findsOneWidget);
-  });
-
-  testWidgets('Create Swiper with list', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
-        home: Swiper.list(
-          list: ["0", "1"],
-          builder: (BuildContext context, dynamic data, int index) {
-            return Text(data);
-          },
-        )));
-
-    expect(find.text("0", skipOffstage: false), findsOneWidget);
-  });
 
 }
